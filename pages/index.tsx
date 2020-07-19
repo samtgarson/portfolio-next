a2e887e08c520d6b6e4dee7b886e22e534f1a496
Sam Garson <samtgarson@gmail.com>
2020-07-19 17:28:40 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
 import { Intro } from '~/components/slides/intro'
-/* import { About } from '~/components/slides/about' */
 import { Highlights } from '~/components/slides/highlights'
 import { ThemeDivider } from '~/components/theme-divider'
 import { Themes } from '~/styles/theme'
-import Ideas from '~/components/slides/ideas'
+import { Ideas } from '~/components/slides/ideas'
 import { Labs } from '~/components/slides/labs'
 import { Contact } from '~/components/slides/contact'
 
